fd47003af35933eabf34db67cf47ee6309b475b6
Vladislav Polyakov <v.polyakov@nv.dunice.net>
2022-11-14 22:16:04 +0300

    fix: Minor changes

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let even = 0;
 (() => {
   for (let i = 0; i < 32; i++) {
     arr.push(arr[i] + arr[i + 1])
-    if (arr[i + 1] % 2 == 0) {
+    if (arr[i + 1] % 2 === 0) {
       even += arr[i + 1];
     }
   }
-})()
+})();
